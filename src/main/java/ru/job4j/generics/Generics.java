f781895d64216c86675222c67a4118aab9e91eed
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-03-06 06:58:30 +0600

    1. Что такое обобщенные типы (generics) [#4952 #484506]

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -17,11 +17,19 @@ public class Generics {
         generics.printObject(second);
         generics.printObject(third);
         System.out.println();
+
+        /**
+         * generics.printBoundedWildCard(first);
+         */
         generics.printBoundedWildCard(second);
         generics.printBoundedWildCard(third);
         System.out.println();
+
         generics.printLowerBoundedWildCard(first);
         generics.printLowerBoundedWildCard(second);
+        /**
+         * generics.printLowerBoundedWildCard(third);
+         */
     }
 
     public void printObject(List<?> list) {
