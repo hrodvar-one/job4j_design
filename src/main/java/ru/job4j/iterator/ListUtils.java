4a0820a089a777bb0e0a9491e0b12821fd18474d
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-03-27 09:37:32 +0600

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,60 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.previousIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T next = iterator.next();
+            if (filter.test(next)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T next = iterator.next();
+            if (filter.test(next)) {
+                iterator.set(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        for (int i = 0; i < list.size(); i++) {
+            for (T element : elements) {
+                if (list.get(i) == element) {
+                    list.remove(list.get(i));
+                }
+            }
+        }
+    }
+}
