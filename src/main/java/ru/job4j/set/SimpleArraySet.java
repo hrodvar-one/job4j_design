1864519a1d749342c7fb4689f296c5e0adca02c0
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-04-02 06:56:55 +0600

    1. Реализовать коллекцию Set на массиве [#996 #489224]

--- src/main/java/ru/job4j/set/SimpleArraySet.java
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -9,10 +9,9 @@ public class SimpleArraySet<T> implements SimpleSet<T> {
 
     @Override
     public boolean add(T value) {
-        boolean result = false;
-        if (set.size() == 0 || !contains(value)) {
+        boolean result = !contains(value);
+        if (result) {
             set.add(value);
-            result = true;
         }
         return result;
     }
