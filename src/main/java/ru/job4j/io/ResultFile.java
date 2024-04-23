fac148af2eaa1ae5353dc2c1aa83fec5e6e0abe5
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-04-23 18:53:43 +0600

    2.0. FileOutputStream [#252488 #492714]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultFile {
     public static void main(String[] args) {
@@ -16,7 +17,7 @@ public class ResultFile {
                             1 * 9 = 9
                             """.getBytes());
             output.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
