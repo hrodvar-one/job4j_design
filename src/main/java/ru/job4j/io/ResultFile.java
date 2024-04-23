b56aea204b6c394676d1945d3db63275a06f38d6
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-04-23 15:02:01 +0600

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            output.write("""
+                            1 * 2 = 2
+                            1 * 3 = 3
+                            1 * 4 = 4
+                            1 * 5 = 5
+                            1 * 6 = 6
+                            1 * 7 = 7
+                            1 * 8 = 8
+                            1 * 9 = 9
+                            """.getBytes());
+            output.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
