b11980eaa001fd9813a94bebf087b352b9a59582
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-04-28 08:38:07 +0600

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,8 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,8 +26,19 @@ public class LogFilter {
         return lines;
     }
 
+    public void saveTo(String out) {
+        var data = filter();
+        try (PrintWriter output = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(out)
+                ))) {
+            output.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter("data/log.txt");
-        logFilter.filter().forEach(System.out::println);
+        new LogFilter("data/log.txt").saveTo("data/404.txt");
     }
 }
--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,22 +1,26 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
-            output.write("""
-                            1 * 2 = 2
-                            1 * 3 = 3
-                            1 * 4 = 4
-                            1 * 5 = 5
-                            1 * 6 = 6
-                            1 * 7 = 7
-                            1 * 8 = 8
-                            1 * 9 = 9
-                            """.getBytes());
-            output.write(System.lineSeparator().getBytes());
+        try (PrintWriter output = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/dataresult.txt")
+                ))) {
+            output.println("""
+                        1 * 2 = 2
+                        1 * 3 = 3
+                        1 * 4 = 4
+                        1 * 5 = 5
+                        1 * 6 = 6
+                        1 * 7 = 7
+                        1 * 8 = 8
+                        1 * 9 = 9
+                        """);
         } catch (IOException e) {
             e.printStackTrace();
         }
