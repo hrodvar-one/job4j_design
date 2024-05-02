9c136f1057b1668dd87c0f347068766511df4f96
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-05-02 08:17:42 +0600

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,29 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validateArgs(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args.length > 2) {
+            throw new IllegalArgumentException("Too many arguments. Should be 2.");
+        }
+        if (!".".equals(args[0])) {
+            throw new IllegalArgumentException("Invalid root folder. Usage  ROOT_FOLDER.");
+        }
+        if (!".js".equals(args[1])) {
+            throw new IllegalArgumentException("Incorrect file extension " + args[1]);
+        }
     }
 }
