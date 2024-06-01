9156b5e2d8feab17529d794742844b0c84a32318
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-06-01 15:45:15 +0600

    3. Отладчик при работе с Базой Данных [#504953 #498856]

--- src/main/java/ru/job4j/jdbc/City.java
+++ src/main/java/ru/job4j/jdbc/City.java
@@ -3,15 +3,24 @@ package ru.job4j.jdbc;
 public class City {
 
     private int id;
-
     private String name;
-
     private int population;
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
 
     public City(int id, String name, int population) {
         this.id = id;
         this.name = name;
         this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("City{id= %s, name= %s, population= %s}",
+                id, name, population);
     }
 
     public int getId() {
