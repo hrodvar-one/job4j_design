3d5404f267559df62c12a90186fbc64bbbea01d5
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-01 14:21:22 +0600

    2. Принципы Kiss, Dry и Yagni [#238813 #503109]

--- src/test/java/ru/job4j/kiss/fool/FoolTest.java
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -7,27 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FoolTest {
 
     @Test
-    void whenNumberIsMultipleOf3And5_thenReturnFizzBuzz() {
+    void whenNumberIsMultipleOf3And5thenReturnFizzBuzz() {
         assertEquals("FizzBuzz", Fool.getExpectedAnswer(15));
         assertEquals("FizzBuzz", Fool.getExpectedAnswer(30));
     }
 
     @Test
-    void whenNumberIsMultipleOf3_thenReturnFizz() {
+    void whenNumberIsMultipleOf3thenReturnFizz() {
         assertEquals("Fizz", Fool.getExpectedAnswer(3));
         assertEquals("Fizz", Fool.getExpectedAnswer(6));
         assertEquals("Fizz", Fool.getExpectedAnswer(9));
     }
 
     @Test
-    void whenNumberIsMultipleOf5_thenReturnBuzz() {
+    void whenNumberIsMultipleOf5thenReturnBuzz() {
         assertEquals("Buzz", Fool.getExpectedAnswer(5));
         assertEquals("Buzz", Fool.getExpectedAnswer(10));
         assertEquals("Buzz", Fool.getExpectedAnswer(20));
     }
 
     @Test
-    void whenNumberIsNotMultipleOf3Or5_thenReturnNumber() {
+    void whenNumberIsNotMultipleOf3Or5thenReturnNumber() {
         assertEquals("1", Fool.getExpectedAnswer(1));
         assertEquals("2", Fool.getExpectedAnswer(2));
         assertEquals("4", Fool.getExpectedAnswer(4));
